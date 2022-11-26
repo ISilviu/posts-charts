00ef2f080bdc348bd1391410c7afd7541b940b69
Ifrim Silviu <silviu.redent@gmail.com>
2022-11-26 16:12:56 +0200

    Finish tests.

--- src/data/posts.test.js
+++ src/data/posts.test.js
@@ -111,5 +111,93 @@ describe('posts functions', () => {
             const histogramData = extractHistogramData(posts);
             expect(histogramData).toBeNull();
         });
+
+        test('histogram data is successfully extracted', () => {
+            const histogramData = extractHistogramData([
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 2,
+                    },
+                },
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 1,
+                    },
+                },
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 8,
+                    },
+                },
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 8,
+                    },
+                },
+            ]);
+
+            expect(histogramData).toStrictEqual([
+                ["January", 0],
+                ["February", 1],
+                ["March", 1],
+                ["April", 0],
+                ["May", 0],
+                ["June", 0],
+                ["July", 0],
+                ["August", 0],
+                ["September", 2],
+                ["October", 0],
+                ["November", 0],
+                ["December", 0],
+            ]);
+        });
+
+        test('out of range months are not added', () => {
+            const histogramData = extractHistogramData([
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 15,
+                    },
+                },
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 16,
+                    },
+                },
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 12,
+                    },
+                },
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: -1,
+                    },
+                },
+            ]);
+
+            expect(histogramData).toStrictEqual([
+                ["January", 0],
+                ["February", 0],
+                ["March", 0],
+                ["April", 0],
+                ["May", 0],
+                ["June", 0],
+                ["July", 0],
+                ["August", 0],
+                ["September", 0],
+                ["October", 0],
+                ["November", 0],
+                ["December", 0],
+            ]);
+        });
     });
 }); 
