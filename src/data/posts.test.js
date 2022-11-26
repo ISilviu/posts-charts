856ed8caf29c182b5f5a66d4c9c089bcb28b53b3
Ifrim Silviu <silviu.redent@gmail.com>
2022-11-26 14:22:44 +0200

    Finalize tests for `transformPosts`.

--- src/data/posts.test.js
+++ src/data/posts.test.js
@@ -14,6 +14,96 @@ describe('posts functions', () => {
             const posts = transformPosts(queryData);
             expect(posts).toBeNull();
         });
+
+        test('query data is properly transformed', () => {
+            const queryData = {
+                allPosts: [
+                    {
+                        createdAt: '1552447290285',
+                    },
+                    {
+                        createdAt: '1551158578751',
+                    },
+                    {
+                        createdAt: '1567711305251',
+                    },
+                ],
+            };
+
+            const posts = transformPosts(queryData);
+            expect(posts).toStrictEqual([
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 2,
+                    },
+                },
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 1,
+                    },
+                },
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 8,
+                    },
+                },
+            ]);
+        });
+
+        test('posts from other years than 2019 are filtered out', () => {
+            const queryData = {
+                allPosts: [
+                    {
+                        createdAt: '1637921828',
+                    },
+                    {
+                        createdAt: '1637921828',
+                    },
+                    {
+                        createdAt: '1567711305251',
+                    },
+                ],
+            };
+
+            const posts = transformPosts(queryData);
+            expect(posts).toStrictEqual([
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 8,
+                    },
+                },
+            ]);
+        });
+
+        test("posts with malformed createdAt are filtered out", () => {
+            const queryData = {
+                allPosts: [
+                    {
+                        createdAt: 'foo',
+                    },
+                    {
+                        createdAt: '123foo',
+                    },
+                    {
+                        createdAt: '1567711305251',
+                    },
+                ],
+            };
+
+            const posts = transformPosts(queryData);
+            expect(posts).toStrictEqual([
+                {
+                    createdAt: {
+                        year: 2019,
+                        month: 8,
+                    },
+                },
+            ]);
+        });
     });
 
     describe('extractHistogramData', () => {
